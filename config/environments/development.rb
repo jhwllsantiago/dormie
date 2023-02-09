7d89228ecd9d04f523a35f3ddd034702eb22f056
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-09 15:32:55 +0800

    owner model, SMTP settings for mailcatcher during development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,8 @@ Rails.application.configure do
 
   # Part of devise installation
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  ## SMTP settings for mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "127.0.0.1", port: 1025 }
 end
