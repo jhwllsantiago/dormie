cbcafd410ca7faf0ac2d222743318a9201a66735
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-14 18:45:11 +0800

    rooms CRUD, location selection

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,11 @@
 class RoomsController < ApplicationController
   before_action :authenticate_owner!, except: %i[ index show ]
   before_action :set_room, only: %i[ show edit update destroy ]
+  before_action :room_ownership, only: %i[ edit update destroy ]
+  before_action :set_locations, only: %i[ new edit ]
 
   def index
-
+    @rooms = Room.all
   end
 
   def show
@@ -16,9 +18,8 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
-    @room.owner = current_owner
-    @room.location = Location.first
+    @room = Room.new(room_params.except(:location))
+    @room.location = Location.find(room_params[:location])
 
     if @room.save
       redirect_to room_url(@room), notice: "Room was successfully created."
@@ -28,24 +29,38 @@ class RoomsController < ApplicationController
   end
 
   def edit
-
+    @tags = Tag.names_to_hash
   end
 
   def update
+    @room.location = Location.find(room_params[:location]) if room_params[:location].present? 
 
+    if @room.update(room_params.except(:location))
+      redirect_to room_url(@room), notice: "Room details was successfully updated."
+    else
+      redirect_to new_room_path, alert: "Room was not created.", status: :unprocessable_entity
+    end
   end
 
   def destroy
-
+    @room.destroy
+    redirect_to new_room_path, notice: "Room was successfully deleted."
   end
 
   private
+  def room_ownership
+    redirect_to root_path unless current_owner&.rooms.include?(@room)
+  end
+
   def set_room
     @room = Room.find(params[:id])
   end
 
   def room_params
-    params.require(:room).permit(:name, :rent, :capacity, :vacancies, :description, tags: [])
+    params.require(:room).permit(:name, :rent, :capacity, :vacancies, :description, :location, tags: [])
   end
 
+  def set_locations
+    @locations = current_owner.locations
+  end
 end
