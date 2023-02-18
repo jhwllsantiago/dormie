dfc60a6f8c1ba9f1e5cdd138b957ab4457351175
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-18 14:35:36 +0800

    refactor reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
-  include ActionView::RecordIdentifier
   before_action :authenticate_occupant!
   before_action :set_room
+  before_action :set_review, only: %i[ edit update destroy ]
 
   def create
     @review = Review.new(review_params)
@@ -16,18 +16,16 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    @review = Review.find(params[:review_id])
+
   end
 
   def update
-    @review = Review.find(params[:review_id])
     @review.update(review_params)
    
     render partial: "reviews/editable_review", locals: { room: @room, review: @review }
   end
 
   def destroy
-    @review = Review.find(params[:review_id])
     @review.destroy
 
     render partial: "reviews/form", locals: { room: @room, review: Review.new, show_cancel_button: false }
@@ -36,6 +34,10 @@ class ReviewsController < ApplicationController
   private
   def set_room
     @room = Room.find(params[:room_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:review_id])
   end
 
   def review_params
