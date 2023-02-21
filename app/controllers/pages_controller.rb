6394c0c126b4e7806bde4d0e3e13d7cee531c578
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-21 18:08:34 +0800

    dashboard page, destroy location, room destroy response

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,21 @@
 class PagesController < ApplicationController
+  before_action :authenticate_owner!, only: %i[ dashboard ]
+
   def home
   end
 
   def options
+  end
+
+  def dashboard
+    @locations = current_owner.locations
+    @rooms = current_owner.rooms.includes(:location)
+    @schedule = current_owner.schedule
+  end
+
+  def map
+    location = Location.find(params[:location_id])
+
+    render partial: "pages/dashboard/map", locals: { query: location.query } 
   end
 end
