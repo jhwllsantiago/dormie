3ba7643348f398c7ed33834f07f2cd00bdcdfea4
Kiara Marie <koodlem@gmail.com>
2023-02-21 17:17:24 +0800

    created method to modify city names

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,12 @@
 module RoomsHelper
+  
+  def shortened_city(city)
+    arr = city.split(" ")
+    if arr.length == 3
+      return "#{arr[2]} #{arr[0]}"
+    else
+      return city
+    end
+  end
+  
 end
