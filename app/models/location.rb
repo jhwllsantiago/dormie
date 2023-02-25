2b3b5a67cbcfe5bdfb2c35a33af6149239de212c
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-25 21:34:54 +0800

    fixed after save geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,11 @@
 class Location < ApplicationRecord
   validates :name, :unit, :address_line, :city, :province, :query, presence: true
   geocoded_by :query
-  after_validation :geocode if :coordinate_blank?
+  after_save :geocode if :coordinate_blank?
   belongs_to :owner
   has_many :rooms, dependent: :destroy
 
   def coordinate_blank?
-    latitude.blank? or longitude.blank?
+    this.latitude.blank? or this.longitude.blank?
   end
 end
