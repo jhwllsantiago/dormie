6db956eb6fe0d1db905d9a09ab106cce0e4235ca
Kiara Marie <koodlem@gmail.com>
2023-02-25 17:08:13 +0800

    Revert "generated mailer"
    This reverts commit 5496b400f303c2f960475ffcdf83590e0e55cd1c.

--- config/environment.rb
+++ config/environment.rb
@@ -4,19 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.application.configure do
-  config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 465,
-    domain: 'gmail.com',
-    user_name: 'dormie.avion@gmail.com',
-    password: 'mnccqjhgytbtkiog',
-    authentication: 'plain',
-    :ssl => true,
-    :tsl => true,
-    enable_starttls_auto: true
-  }
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
 end
