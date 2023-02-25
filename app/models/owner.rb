29767860ac3a3d0d4c2bc8b7989cecea4723df2f
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-02-25 16:17:57 +0800

    removed schedule feature

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -7,6 +7,5 @@ class Owner < ApplicationRecord
   validates :name, presence: true
   has_many :locations, dependent: :destroy
   has_many :rooms, through: :locations
-  has_one :schedule, dependent: :destroy
   enum :status, { unverified: 0, verified: 1, partner: 2, inactive: 3 }
 end
