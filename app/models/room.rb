106f89d794292d45ebcfe82f82a8c87222332074
Kiara Marie <koodlem@gmail.com>
2023-02-25 19:16:01 +0800

    image resizing for carousel

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,11 +5,11 @@ class Room < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many_attached :images, dependent: :destroy
 
-  def image_as_thumbnail image
+  def image_as_thumbnail image, width=400, height=400
     # libvips is required for image variants
     # disable line 11 if production server supports installation of libvips
     return image if Rails.env.production?
     return unless image.content_type.in?(%w[ image/jpeg image/png ])
-    image.variant(resize_to_limit: [400, 400]).processed
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
