26d1090c05e9115fad2b2f3eec8b4bdc7c7ee864
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-01 18:05:33 +0800

    removed shortened city helper

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,17 +1,6 @@
 module RoomsHelper
   include Pagy::Frontend
   
-  def shortened_city(city)
-    arr = city.split(" ")
-    if arr.length == 3
-      return "#{arr[2]} #{arr[0]}"
-    elsif arr.length == 4
-      return "#{arr[2]} #{arr[3]} #{arr[0]}"
-    else
-      return city
-    end
-  end
-  
   def extract_sort_option param
     order = param&.split("-")&.map(&:to_sym)
     if param.blank? or %i[ distance rent updated_at ].exclude?(order[0]) or %i[ asc desc ].exclude?(order[1])
