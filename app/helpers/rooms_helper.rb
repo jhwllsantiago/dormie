38eb4c385364c0bef3fbc713e2458a36e4e518f5
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-01 15:15:39 +0800

    prevent invalid order params

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -13,7 +13,11 @@ module RoomsHelper
   end
   
   def extract_sort_option param
-    return [:updated_at, :desc] if param.blank?
-    param.split("-").map(&:to_sym)
+    order = param.split("-").map(&:to_sym)
+    if param.blank? or %i[ distance rent updated_at ].exclude?(order[0]) or %i[ asc desc ].exclude?(order[1])
+      [:updated_at, :desc]
+    else
+      order
+    end
   end
 end
