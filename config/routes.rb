d56a7ea66530233600497b9b1feb45a39d3f85d1
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-01 18:12:03 +0800

    removed psgc related

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   get "locations/new", to: "locations#new", as: "new_location"
   post "locations", to: "locations#create"
   delete "locations/:id", to: "locations#destroy", as: "location"
-  get "locations/select-barangay", to: "locations#select_barangay", as: "select_barangay"
   get "/sign_in", to: "pages#options"
 
   post "room/:room_id/reviews", to: "reviews#create", as: "room_reviews"
