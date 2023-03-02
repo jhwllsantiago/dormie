2ddd79199b523d84e14820431b199897f8fc0c8c
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-02 22:30:58 +0800

    added maps request spec, location factory

--- spec/requests/maps_spec.rb
+++ spec/requests/maps_spec.rb
@@ -1,7 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "Maps", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET results_map" do
+    it "is succesful" do
+      get results_map_path
+      expect(response).to be_successful
+    end
+  end
+
+  context "owner is signed in" do
+    let(:owner) {create(:owner)}
+    before { sign_in owner }
+
+    describe "GET location_map" do
+      let(:location) {create(:location, owner: owner)}
+      it "raises an error" do
+        expect { get location_map_path(1) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it "is succesful" do
+        get location_map_path(location.id)
+        expect(response).to be_successful
+        expect(response.body).to include('<turbo-frame id="location_map_frame">')
+      end
+    end
+
+    # describe "GET location_preview" do
+    #   it "is succesful" do
+    #     get location_preview_path
+    #     expect(response).to be_successful
+    #     expect(response.media_type).to eq Mime[:turbo_stream]
+    #   end
+    # end
+
+    describe "GET marker_map" do
+      it "is succesful" do
+        get marker_map_path
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  context "owner is not signed in" do
+    describe "GET location_map" do
+      it "redirects to new_owner_session" do
+        get location_map_path(1)
+        expect(response).to redirect_to(new_owner_session_path)
+      end
+    end
+
+    describe "GET location_preview" do
+      it "redirects to new_owner_session" do
+        get location_preview_path
+        expect(response).to redirect_to(new_owner_session_path)
+      end
+    end
+
+    describe "GET marker_map" do
+      it "redirects to new_owner_session" do
+        get marker_map_path
+        expect(response).to redirect_to(new_owner_session_path)
+      end
+    end
   end
 end
