ebda1ad6addef0381b7f1a493e5fd8a06ea987e0
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-03 17:30:44 +0800

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 465,
+    domain: "gmail.com",
+    user_name: ENV["USERNAME"],
+    password: ENV["PASSWORD"],
+    authentication: "plain",
+    :ssl => true,
+    :tsl => true,
+    enable_starttls_auto: true
+  }
 end
