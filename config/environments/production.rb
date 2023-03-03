aca52326c79491312d1f5978527322925faad396
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-03 18:04:12 +0800

    production missing host potential fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  
+  config.action_mailer.default_url_options = { host: "dormie.onrender.com", protocol: "https" }
   config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
