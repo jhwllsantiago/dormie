ecef99824b64f01deb6dfe3c20e14a1386c8ca71
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-03-12 23:46:48 +0800

    added reviews authorization

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -4,4 +4,8 @@ module RoomsHelper
   def notify?
     params[:page].blank? and params[:commit] == "Search"
   end
+
+  def allowed_to_write_review?
+    occupant_signed_in? and @room.owner.email != current_occupant&.email
+  end
 end
